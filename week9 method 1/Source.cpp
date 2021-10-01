4d251ed43ceba9bdc33d3c1cf4d31de00f4b723d
Parinthorn Kanurak <parinthorn112545@gmail.com>
2021-10-01 23:42:27 +0700

    vowel print

--- week9 method 1/Source.cpp
+++ week9 method 1/Source.cpp
@@ -9,14 +9,14 @@ int main() {
 
 	printf("Input 1 sentence : ");
 	fgets(x, 1000, stdin);
-	int count[5] = {0,0,0,0,0};
-	int i=0;
+	int count[5] = { 0,0,0,0,0 };
+	int i = 0;
 	while (x[i] != NULL) {
 		if (x[i] == 'a' || x[i] == 'A') {
 			count[0] += 1;
 			i++;
 		}
-		else if (x[i] == 'e' || x[i] =='E') {
+		else if (x[i] == 'e' || x[i] == 'E') {
 			count[1] += 1;
 			i++;
 		}
@@ -35,10 +35,23 @@ int main() {
 		else { i++; }
 	}
 	printf("Vowels list \n");
-	printf(" a or A : %d \n", count[0]);
-	printf(" e or E : %d \n", count[1]);
-	printf(" i or I : %d \n", count[2]);
-	printf(" o or O : %d \n", count[3]);
-	printf(" u or U : %d \n", count[4]);
+	if (count[0] != 0) {
+		printf(" a or A : %d \n", count[0]);
+	}
+	if (count[1] != 0) {
+		printf(" e or E : %d \n", count[1]);
+	}
+	if (count[2] != 0) {
+		printf(" i or I : %d \n", count[2]);
+	}
+	if (count[3] != 0) {
+		printf(" o or O : %d \n", count[3]);
+	}
+	if (count[4] != 0) {
+		printf(" u or U : %d \n", count[4]);
+	}
+	if (count[0]==0&& count[1] == 0 && count[2] == 0 && count[3] == 0 && count[4] == 0 ) {
+		printf("No vowel\n");
+	}
 	return 0;
 }
